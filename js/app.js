4961f145e626fd9946fae0c48fa0d727473250a2
johanrilke <johanrilke.fe@gmail.com>
2023-05-01 17:18:40 +0700

    Add _displayCaloriesTotal and _render func

--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
 class CalorieTracker {
     constructor() {
+        // Base variables
+
         this._calorieLimit = 2000;
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
+
+        // Run Methods upon Class Instantiation
+        
+        this._displayCaloriesTotal();
     }
+
+    // Public Methods
 
     addMeal(meal) {
         this._meals.push(meal);
         this._totalCalories += meal.calories;
+        this._render();
     }
 
     addWorkout(workout) {
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
+        this._render();
+    }
+
+    // Private Methods
+    
+    _displayCaloriesTotal() {
+        const totalCaloriesEl = document.querySelector('#calories-total');
+        totalCaloriesEl.innerHTML = this._totalCalories;
+    }
+
+    _render() {
+        this._displayCaloriesTotal();
     }
 }
 
@@ -36,7 +57,9 @@ class Workout {
 const tracker = new CalorieTracker();
 
 const breakfast = new Meal('Breakfast', 400);
+const lunch = new Meal('Lunch', 350);
 tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
 
 const run = new Workout('Morning Run', 300);
 tracker.addWorkout(run);
