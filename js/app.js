4ee088487896e8181815a8a26123c6c56e5b61de
johanrilke <johanrilke.fe@gmail.com>
2023-05-02 11:13:10 +0700

    Add Set Limit method to App and Base Tracker Class

--- js/app.js
+++ js/app.js
@@ -61,6 +61,12 @@ class CalorieTracker {
         this._render();
     }
 
+    setLimit(calorieLimit) {
+        this._calorieLimit = calorieLimit;
+        this._displayCaloriesLimit();
+        this._render();
+    }
+
     // Private Methods
     
     _displayCaloriesTotal() {
@@ -217,6 +223,9 @@ class App {
 
         document.getElementById('reset')
             .addEventListener('click', this._reset.bind(this));
+
+        document.getElementById('limit-form')
+            .addEventListener('submit', this._setLimit.bind(this));
     }
 
     _newItem(type, e) {
@@ -284,6 +293,24 @@ class App {
         document.getElementById('filter-meals').innerHTML = '';
         document.getElementById('filter-workouts').innerHTML = '';
 
+    }
+
+    _setLimit(e) {
+        e.preventDefault();
+
+        const limit = document.getElementById('limit');
+
+        if (limit.value === '') {
+            alert('Please add a limit');
+            return;
+        }
+
+        this._tracker.setLimit(+limit.value);
+        limit.value = '';
+        
+        const modalEl = document.getElementById('limit-modal');
+        const modal = bootstrap.Modal.getInstance(modalEl);
+        modal.hide();
     }
 }
 
