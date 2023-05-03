e874fad74fa2415a9f24fe03eed656cd1dda7bf4
johanrilke <johanrilke.fe@gmail.com>
2023-05-03 09:28:36 +0700

    Persist Workouts to Local Storage and Display

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ class CalorieTracker {
         this._calorieLimit = Storage.getCalorieLimit();
         this._totalCalories = Storage.getTotalCalories(0);
         this._meals = Storage.getMeals();
-        this._workouts = [];
+        this._workouts = Storage.getWorkouts();
 
         // Run Methods upon Class Instantiation
         
@@ -33,6 +33,7 @@ class CalorieTracker {
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
         Storage.updateTotalCalories(this._totalCalories);
+        Storage.saveWorkout(workout);
         this._displayNewWorkout(workout);
         this._render();
     }
@@ -75,6 +76,7 @@ class CalorieTracker {
 
     loadItems() {
         this._meals.forEach((meal) => this._displayNewMeal(meal));
+        this.workouts.forEach((workout) => this._displayNewWorkout(workout));
     }
 
     // Private Methods
@@ -250,6 +252,22 @@ class Storage {
         const meals = Storage.getMeals();
         meals.push(meal);
         localStorage.setItem('meals', JSON.stringify(meals));
+    }
+
+    static getWorkouts() {
+        let workouts;
+        if (localStorage.getItem('workouts') === null) {
+            workouts = [];
+        } else {
+            workouts = JSON.parse(localStorage.getItem('workouts'));
+        }
+        return workouts;
+    }
+
+    static saveWorkout(workout) {
+        const workouts = Storage.getWorkouts();
+        workouts.push(workout);
+        localStorage.setItem('workouts', JSON.stringify(workouts));
     }
 }
 
