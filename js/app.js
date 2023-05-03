67e32737004d174628d5d2d1b7f67337504e61d4
johanrilke <johanrilke.fe@gmail.com>
2023-05-03 09:05:22 +0700

    Persist Total Calories to Local Storage
    This includes the addition of the following methods:
    - static Storage method to get the total calories
    - static Storage method to update the total calories
    - call the update method in the Base Tracker's public method whenever there is change (add or remove) to meals or workouts

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ class CalorieTracker {
         // Base variables
 
         this._calorieLimit = Storage.getCalorieLimit();
-        this._totalCalories = 0;
+        this._totalCalories = Storage.getTotalCalories(0);
         this._meals = [];
         this._workouts = [];
 
@@ -23,6 +23,7 @@ class CalorieTracker {
     addMeal(meal) {
         this._meals.push(meal);
         this._totalCalories += meal.calories;
+        Storage.updateTotalCalories(this._totalCalories);
         this._displayNewMeal(meal);
         this._render();
     }
@@ -30,6 +31,7 @@ class CalorieTracker {
     addWorkout(workout) {
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
+        Storage.updateTotalCalories(this._totalCalories);
         this._displayNewWorkout(workout);
         this._render();
     }
@@ -39,6 +41,7 @@ class CalorieTracker {
         if (index !== -1) {
             const meal = this._meals[index];
             this._totalCalories -= meal.calories;
+            Storage.updateTotalCalories(this._totalCalories);
             this._meals.splice(index, 1);
             this._render();
         }
@@ -49,6 +52,7 @@ class CalorieTracker {
         if (index !== -1) {
             const workout = this._workouts[index];
             this._totalCalories += workout.calories;
+            Storage.updateTotalCalories(this._totalCalories);
             this._workouts.splice(index, 1);
             this._render();
         }
@@ -211,6 +215,20 @@ class Storage {
 
     static setCalorieLimit(calorieLimit) {
         localStorage.setItem('calorieLimit', calorieLimit);
+    }
+
+    static getTotalCalories(defaultCalories = 0) {
+        let totalCalories;
+        if (localStorage.getItem('totalCalories') === null) {
+            totalCalories = defaultCalories;
+        } else {
+            totalCalories = +localStorage.getItem('totalCalories');
+        }
+        return totalCalories;
+    }
+
+    static updateTotalCalories(calories) {
+        localStorage.setItem('totalCalories', calories);
     }
 }
 
